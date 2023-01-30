a9483b47c9c7fa446a8af9e2886b40fe872bf98a
wanglizhen917 <2903127702@qq.com>
2023-01-30 11:07:02 +0800

    feat: add login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
 import { ProjectListScreen } from 'screens/project-list'
+import { TsReactTest } from 'screens/project-list/test'
+import { Login } from 'screens/login'
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      {/* <TsReactTest /> */}
+      <Login />
     </div>
   )
 }
---
+++ src/screens/project-list/test.tsx
@@ -0,0 +1,88 @@
+import React, { useCallback, useState } from 'react'
+import { useMount } from '.'
+
+interface PersonProps {
+  name: string
+  age: number
+}
+
+export const TsReactTest = () => {
+  const persons: PersonProps[] = [
+    { name: 'jack', age: 25 },
+    { name: 'dd', age: 34 },
+  ]
+
+  const { value, clear, removeIndex, add } = useArray(persons)
+
+  useMount(() => {
+    //console.log(value.notExist)
+    //add({name:'ssss'})
+    //removeIndex("123")
+  })
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          add({ name: 'test', age: 34 })
+        }}
+      >
+        Add
+      </button>
+      <button
+        onClick={() => {
+          removeIndex(0)
+        }}
+      >
+        Remove first
+      </button>
+      <button
+        onClick={() => {
+          clear()
+        }}
+      >
+        Clear
+      </button>
+      <ul>
+        {value.map(({ name, age }, i) => {
+          return (
+            <li key={i}>
+              {i} - {name} - {age}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+const useArray = <T,>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray)
+
+  const add = useCallback(
+    (item: T) => {
+      setValue([...value, item])
+    },
+    [value],
+  )
+
+  const removeIndex = useCallback(
+    (index: number) => {
+      const copy = [...value]
+      copy.splice(index, 1)
+      setValue(copy)
+    },
+    [value],
+  )
+
+  const clear = useCallback(() => {
+    setValue([])
+  }, [])
+
+  return {
+    value,
+    clear,
+    removeIndex,
+    add,
+  }
+}
