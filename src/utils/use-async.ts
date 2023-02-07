95195aa51be21504123fb76323b9693a17a1ffe2
wanglizhen917 <2903127702@qq.com>
2023-02-07 15:28:03 +0800

    feat: add useReducer

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useReducer, useRef, useState } from 'react'
 import { useMountedRef } from 'utils'
 
 interface State<D> {
@@ -17,39 +17,56 @@ const defaultConfig = {
   throwOnError: false,
 }
 
+const useSafeDispatch = <T>(
+  dispatch: (...args: T[]) => void,
+) => {
+  const mountedRef = useMountedRef()
+  return useCallback(
+    (...args: T[]) =>
+      mountedRef.current ? dispatch(...args) : void 0,
+    [dispatch, mountedRef],
+  )
+}
+
 export const useAsync = <D>(
   initialState?: State<D>,
   initialConfig?: typeof defaultConfig,
 ) => {
   const config = { ...defaultConfig, ...initialConfig }
-  const [state, setState] = useState<State<D>>({
-    ...defaultInitialState,
-    ...initialState,
-  })
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      ...defaultInitialState,
+      ...initialState,
+    },
+  )
 
-  const mountedRef = useMountedRef()
+  const safeDispatch = useSafeDispatch(dispatch)
 
   //useState直接传入函数的含义是：惰性初始化；所以，要用useState保存函数，不能直接传入函数()=>{}，而是要()=>()=>{}
   const [retry, setRetry] = useState(() => () => {})
 
   const setData = useCallback(
     (data: D) =>
-      setState({
+      safeDispatch({
         data,
         stat: 'success',
         error: null,
       }),
-    [],
+    [safeDispatch],
   )
 
   const setError = useCallback(
     (error: Error) =>
-      setState({
+      safeDispatch({
         error: error,
         stat: 'error',
         data: null,
       }),
-    [],
+    [safeDispatch],
   )
 
   const run = useCallback(
@@ -66,10 +83,10 @@ export const useAsync = <D>(
 
       //会造成无限循环，因为这state改变了，依赖项里又有state，就会循环.解决办法是，不要用到state，使用函数式写法
       //setState({ ...state, stat: 'loading' })
-      setState((prev) => ({ ...prev, stat: 'loading' }))
+      safeDispatch({ stat: 'loading' })
       return promise
         .then((data) => {
-          if (mountedRef.current) setData(data)
+          setData(data)
           return data
         })
         .catch((error) => {
@@ -78,7 +95,7 @@ export const useAsync = <D>(
           return error
         })
     },
-    [config.throwOnError, mountedRef, setData, setError],
+    [config.throwOnError, safeDispatch, setData, setError],
   )
 
   return {
